934d0d7266bbac50c178999b06fa8eb4368bfa59
Timothy-Dement <tmdement@ncsu.edu>
2018-04-11 22:57:25 -0400

    Finish upgrade code

--- provisioners/itrust-cluster.sh
+++ provisioners/itrust-cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for instance in alpha bravo
+for instance in "alpha" "bravo" "charlie" "delta" "echo"
 do
     node "/home/ubuntu/JenkinsDeploy/provisioners/aws-itrust-$instance.js"
 done
